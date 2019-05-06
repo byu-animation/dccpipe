3d4b3af9f7ed4d790861bc00e270a91d2dd10269
mswindler <matthew@ionixdesign.com>
2019-05-06 16:19:10 -0600

    Add to reload_scripts

--- pipe/tools/maya/utils/reload_scripts.py
+++ pipe/tools/maya/utils/reload_scripts.py
@@ -1,5 +1,8 @@
 from pipe.tools.maya import *
 from pipe.tools.maya.cloners import cloner
+from pipe.tools.maya.publishers import publisher as maya_publisher
+from pipe.tools.general import prompts
+from pipe.tools.general import gui_tool
 from pipe.tools.general import publisher
 
 class ReloadScripts:
@@ -7,6 +10,10 @@ class ReloadScripts:
     def go(self):
     	reload(utils.reload_scripts)
     	reload(cloners.cloner)
+        reload(prompts)
+        reload(gui_tool)
+        reload(publisher)
+        reload(maya_publisher)
     	# reload(exporters.exporter)
     	# reload(exporters.tagger)
     	# reload(importers.referencer)
