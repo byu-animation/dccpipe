b733ab3b00decf6b1409346c8e1c97405561ae2f
mswindler <matthew@ionixdesign.com>
2019-07-10 17:36:17 -0600

    Progress on mayacloner

--- pipe/tools/mayatools/cloners/cloner.py
+++ pipe/tools/mayatools/cloners/cloner.py
@@ -4,6 +4,8 @@ from pipe.gui.checkout_gui import CheckoutWindow
 import pipe.gui.select_from_list as select_gui
 import pipe.tools.mayatools.utils.utils as maya_utils
 from pipe.am.project import Project
+from pipe.am.body import Body
+from pipe.am.element import Element
 
 from PySide2 import QtWidgets
 import maya.cmds as mc
@@ -50,24 +52,40 @@ class MayaCloner:
 			mc.file(filePath, open=True, force=True)
 			print "open file " + assetName
 		else:
-			print 'Does not Exist: '+assetName
+			print 'File does not exist: '+assetName
 
 	def go(self):
 		project = Project()
 		asset_list = project.list_assets()
-		asset_list = ['one', 'two']
-		self.item_gui = select_gui.SelectFromList(l=asset_list, parent=maya_utils.maya_main_window())
+		# asset_list = ['one', 'two']
+		self.item_gui = select_gui.SelectFromList(l=asset_list, parent=maya_utils.maya_main_window(), title="Select an asset to clone")
 		self.item_gui.submitted.connect(self.results)
-		self.item_gui.show()
 
 	def results(self, value):
 		print("Final value: ", value[0])
-		# TODO do something with the selected value
+		filename = value[0]
 
-	# def go(self):
-	# 	parent = maya_utils.maya_main_window()
-	# 	# TODO: replace temp depts with actual options for departments (loaded from config file)
-	# 	self.maya_checkout_dialog = CheckoutWindow(parent, ["temp_dept_1", "temp_dept_2"])
-	# 	self.maya_checkout_dialog.finished.connect(self.open_file)
-	# 	# if dialog.exec_():
-	# 	#	 print self.result
+		# TODO: convert the filename into the full file path
+		project = Project()
+		body = project.get_body(filename)
+
+		# TODO: get the element for the model dept, and using that get the commit. Use these to clone the object
+		element = body.get_element("model")
+
+		filepath = body.get_filepath()
+		print("path: ", filepath)
+
+		if filepath is not None:
+			if not mc.file(q=True, sceneName=True) == '':
+				mc.file(save=True, force=True) #save file
+
+			if not os.path.exists(filepath):
+				mc.file(new=True, force=True)
+				mc.file(rename=filepath)
+				mc.file(save=True, force=True)
+				print "New file: " + filepath
+			else:
+				mc.file(filepath, open=True, force=True)
+				print "File opened: " + filepath
+
+		# TODO do something with the selected value: Get the alembic or .mb file and open in Maya
