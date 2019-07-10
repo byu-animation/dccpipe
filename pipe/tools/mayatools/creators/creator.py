acb70014b813c0f3c571187711ab376b8a07ec59
mswindler <matthew@ionixdesign.com>
2019-07-10 17:36:27 -0600

    Progress on mayacreator

--- pipe/tools/mayatools/creators/creator.py
+++ pipe/tools/mayatools/creators/creator.py
@@ -1,4 +1,5 @@
 import pipe.gui.quick_dialogs as qd
+from pipe.am.project import Project
 
 
 '''
@@ -15,14 +16,18 @@ class Creator():
     '''
     def create_body(self):
         name = qd.input("What's the name of this asset?")
+        type = qd.input("What's the type?")
 
         # determine if asset was created or not.
         created = True
 
-        if name is None:
+        if name is None or type is None:
             created = False
 
         if created:
+            project = Project()
+            project.create_asset(name, asset_type=type)
+            # TODO: something's not quite right here, as the .mb file is not created or saved. Also there should be an intitial commit.
             qd.info("Asset created successfully (but not really, yet).", "Success")
         else:
             qd.error("Asset creation failed.")
