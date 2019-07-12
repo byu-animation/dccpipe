e502d4ae826b39da085fbd77aeb1d3f2f8b8d6f5
mswindler <matthew@ionixdesign.com>
2019-07-12 12:12:22 -0600

    Select asset from list

--- pipe/tools/mayatools/creators/creator.py
+++ pipe/tools/mayatools/creators/creator.py
@@ -3,6 +3,7 @@ import pipe.gui.select_from_list as sfl
 from pipe.am.project import Project
 from pipe.am.environment import Environment
 from pipe.am.body import Body
+from pipe.am.body import AssetType
 from pipe.tools.mayatools.utils.utils import *
 from PySide2 import QtWidgets
 
@@ -14,14 +15,26 @@ Parent class for managing assets
 class Creator:
 
     def __init__(self):
-        pass
+        self.name = None
 
     '''
     This will bring up the create new body UI
     '''
     def create_body(self):
-        name = qd.input("What's the name of this asset?")
-        type = qd.input("What's the type?")
+        self.name = qd.input("What's the name of this asset?")
+
+        if self.name is None:
+            return
+
+        asset_type_list = AssetType().list_asset_types()
+
+        self.item_gui = sfl.SelectFromList(l=asset_type_list, parent=maya_main_window(), title="What are you creating?", width=250, height=120)
+        self.item_gui.submitted.connect(self.results)
+
+    def results(self, value):
+        print("Final value: ", value[0])
+        type = value[0]
+        name = self.name
 
         # determine if asset was created or not.
         created = True
@@ -34,10 +47,8 @@ class Creator:
             body = project.create_asset(name, asset_type=type)
             if body == None:
                 # print a message about failure/duplicate
-                qd.error("Body not found, publish failed.")
+                qd.error("Asset not found, publish failed.")
             else:
-                # correct so far
-                # TODO: publish
                 prepare_scene_file()
                 # show the gui, get the element. To list elements, get the body and get the department
                 department = "model"  # hard-coding model for now since this is Maya
