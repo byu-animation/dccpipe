2e6fb9a4d13029aba8b31ec5f25adcb31ec08501
mswindler <matthew@ionixdesign.com>
2019-08-01 14:47:20 -0600

    Add tagger and fix issues

--- pipe/tools/mayatools/exporters/tagger.py
+++ pipe/tools/mayatools/exporters/tagger.py
@@ -1,3 +1,5 @@
+from pymel.core import *
+from pipe.am.gui import quick_dialogs as qd
 
 
 class Tagger:
@@ -5,4 +7,35 @@ class Tagger:
         pass
 
     def tag(self):
-        print("Tagging? What's that?")
+        self.tagGeo()
+
+    def tagGeo(self):
+    	selected_groups = ls(sl=True, tr=True)
+    	print selected_groups
+    	# response = showConfirmationPopup(selected_groups)
+
+        response = qd.binary_option("Add Alembic tag to:\n" + str(selected_groups), "Yes", "No", title='Add Alembic Tag')
+
+    	if response:
+    		for obj in selected_groups:
+    			if not obj.hasAttr("DCC_Alembic_Export_Flag"):
+    				cmds.lockNode(str(obj), l=False)  # node must be unlocked to add an attribute
+    				obj.addAttr("DCC_Alembic_Export_Flag", dv=True, at=bool, h=False, k=True)
+
+    		qd.info("tag successful!")
+
+    # def showConfirmationPopup(self, selected_groups):
+    # 	return cmds.confirmDialog( title         = 'Add Alembic Tag'
+    # 		                         , message       = 'Add Alembic Tag to:\n' + str(selected_groups)
+    # 		                         , button        = ['Yes', 'No']
+    # 		                         , defaultButton = 'Yes'
+    # 		                         , cancelButton  = 'No'
+    # 		                         , dismissString = 'No')
+    #
+    # def showSuccessPopup(self):
+    # 	return cmds.confirmDialog( title         = 'Success'
+    # 		                         , message       = 'Alembic Tags were successfully added.'
+    # 		                         , button        = ['OK']
+    # 		                         , defaultButton = 'OK'
+    # 		                         , cancelButton  = 'OK'
+    # 		                         , dismissString = 'OK')
