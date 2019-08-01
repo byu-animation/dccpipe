38f6ebf5b4a0eb15cbb740f4d9d3f962a6ef2725
mswindler <matthew@ionixdesign.com>
2019-08-01 15:13:03 -0600

    fix indent

--- pipe/tools/mayatools/exporters/tagger.py
+++ pipe/tools/mayatools/exporters/tagger.py
@@ -10,14 +10,14 @@ class Tagger:
         self.tagGeo()
 
     def tagGeo(self):
-    	selected_groups = ls(sl=True, tr=True)
+        selected_groups = ls(sl=True, tr=True)
 
         response = qd.binary_option("Add Alembic tag to:\n" + str(selected_groups), "Yes", "No", title='Add Alembic Tag')
 
-    	if response:
-    		for obj in selected_groups:
-    			if not obj.hasAttr("DCC_Alembic_Export_Flag"):
-    				cmds.lockNode(str(obj), l=False)  # node must be unlocked to add an attribute
-    				obj.addAttr("DCC_Alembic_Export_Flag", dv=True, at=bool, h=False, k=True)
+        if response:
+            for obj in selected_groups:
+                if not obj.hasAttr("DCC_Alembic_Export_Flag"):
+                    cmds.lockNode(str(obj), l=False)  # node must be unlocked to add an attribute
+                    obj.addAttr("DCC_Alembic_Export_Flag", dv=True, at=bool, h=False, k=True)
 
-    		qd.info("tag successful!")
+            qd.info("tag successful!")
