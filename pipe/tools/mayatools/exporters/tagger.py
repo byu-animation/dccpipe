5d13284b810bfe7b557a0a1f662f49acde248842
mswindler <matthew@ionixdesign.com>
2019-08-02 16:46:01 -0600

    Abstracting functionality from tagger

--- pipe/tools/mayatools/exporters/tagger.py
+++ pipe/tools/mayatools/exporters/tagger.py
@@ -5,26 +5,31 @@ from pipe.tools.mayatools.utils.utils import *
 
 class Tagger:
     def __init__(self):
-        pass
+        self.selected_string = self.get_selected_string()
 
     def tag(self):
-        selected = ls(sl=True, tr=True)
-
-        response = qd.binary_option("Add Alembic tag to:\n" + str(selected_groups), "Yes", "No", title='Add Alembic Tag')
+        response = qd.binary_option("Add Alembic tag to:\n" + str(self.selected_string), "Yes", "No", title='Add Alembic Tag')
 
         if response:
-            for node in selected:
+            for node in self.selected:
                 tag_node_with_flag(node, "DCC_Alembic_Export_Flag")
 
             qd.info("tag successful!")
 
     def untag(self):
-        selected = ls(sl=True, tr=True)
-
-        response = qd.binary_option("Add Alembic tag to:\n" + str(selected_groups), "Yes", "No", title='Add Alembic Tag')
+        response = qd.binary_option("Remove Alembic tag from:\n" + self.selected_string, "Yes", "No", title='Remove Alembic Tag')
 
         if response:
-            for node in selected:
+            for node in self.selected:
                 untag_node_with_flag(node, "DCC_Alembic_Export_Flag")
 
-            qd.info("tag successful!")
+            qd.info("untag successful!")
+
+    def get_selected_string(self):
+        self.selected = ls(sl=True, tr=True)
+        selected_string = ""
+
+        for node in self.selected:
+            selected_string += node
+
+        return selected_string
