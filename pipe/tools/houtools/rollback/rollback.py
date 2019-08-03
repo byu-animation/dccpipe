f514bf3c6e650ce2813135e950114497ed6cfe72
mswindler <matthew@ionixdesign.com>
2019-08-03 13:28:12 -0600

    Skeleton for rollback

---
+++ pipe/tools/houtools/rollback/rollback.py
@@ -0,0 +1,16 @@
+import *  # Something
+
+
+class Rollback:
+
+    def __init__(self):
+        pass
+
+    def rollback_asset(self, node=None):
+        pass
+
+    def rollback_tool(self, node=None):
+        pass
+
+    def rollback_shot(self):
+        pass
