832eed01bb8b119768dad4480da5d63e9953f128
mswindler <matthew@ionixdesign.com>
2019-08-14 18:12:21 -0600

    Various cleanup in houtools

--- pipe/tools/houtools/utils/reload_scripts.py
+++ pipe/tools/houtools/utils/reload_scripts.py
@@ -1,12 +1,8 @@
-# from pipe.tools.houtools import *
+from pipe.tools.houtools import *
 from pipe.tools.houtools.creator import creator as hou_creator
 from pipe.tools.houtools.utils import utils as hou_utils
-# from pipe.tools.houtools.exporters import alembic_exporter as alembic_exporter
-# from pipe.tools.houtools.exporters import json_exporter as json_exporter
-# from pipe.tools.houtools.importers import referencer as hou_referencer
-# from pipe.tools.houtools.exporters import tagger as hou_tagger
-
 from pipe.tools.houtools.assembler import assembler as hou_assembler
+from pipe.tools.houtools.importer import importer as hou_importer
 from pipe.tools.houtools.publisher import publisher as hou_publisher
 from pipe.tools.houtools.cloner import cloner as hou_cloner
 import pipe.gui.quick_dialogs as qd
@@ -15,14 +11,11 @@ import pipe.gui.quick_dialogs as qd
 class ReloadScripts:
 
     def run(self):
+        reload(utils.reload_scripts)
         reload(hou_utils)
         reload(hou_assembler)
         reload(hou_creator)
         reload(hou_cloner)
         reload(hou_publisher)
-        # reload(hou_utils)
+        reload(hou_importer)
         reload(qd)
-        # reload(alembic_exporter)
-        # reload(json_exporter)
-        # reload(hou_referencer)
-        # reload(hou_tagger)
