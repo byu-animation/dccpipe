2bdeed5d31af3d25b20b16294bcd6b1e54ee54f0
mswindler <matthew@ionixdesign.com>
2019-08-26 15:03:30 -0600

    Fix reference geo importer and add error checking

--- pipe/tools/mayatools/importers/reference_importer.py
+++ pipe/tools/mayatools/importers/reference_importer.py
@@ -12,12 +12,16 @@ class ReferenceImporter:
 
     def go(self):
         environment = Environment()
-        reference_dir = environment.get_reference_geo_dir()
-        files = os.listdir(reference_dir)
+        self.reference_dir = environment.get_reference_geo_dir()
+        files = os.listdir(self.reference_dir)
 
         self.item_gui = sfl.SelectFromList(l=files, parent=maya_main_window(), title="Select reference actor(s) to import", multiple_selection=True)
         self.item_gui.submitted.connect(self.results)
 
     def results(self, files):
         for file in files:
-            cmds.file( file, i = True, type="OBJ", iv=True, ra=True, mnc=False, ns="reference_object", op="mo=1", pr=True)
+            try:
+                path = os.path.join(self.reference_dir, file)
+                cmds.file( path, i = True, type="OBJ", iv=True, ra=True, mnc=False, ns="reference_object", op="mo=1", pr=True)
+            except Exception as e:
+                qd.warning(str(e))
