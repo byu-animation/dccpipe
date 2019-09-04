0cf357870444ab4c5f10200c8fb635b3168f9668
mswindler <matthew@ionixdesign.com>
2019-09-04 17:28:20 -0600

    Fix #89

--- launch/unix/sbs.sh
+++ launch/unix/sbs.sh
@@ -2,3 +2,9 @@
 SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SOURCEDIR/env.sh
 export CURRENT_PROGRAM="sbs"
+
+export SUBSTANCE_PLUGINS_DIR=${MEDIA_PROJECT_DIR}'/pipe/tools/sbstools/_custom/'
+export SUBSTANCE_ICONS_DIR=${MEDIA_PROJECT_DIR}'/pipe/tools/_resources/'
+
+echo "Starting Substance..."
+substancepainter
