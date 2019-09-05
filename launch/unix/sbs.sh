0af3003b8b9b413ede25be3eff8d603bd5caa519
mswindler <matthew@ionixdesign.com>
2019-09-05 14:59:04 -0600

    cont #89

--- launch/unix/sbs.sh
+++ launch/unix/sbs.sh
@@ -5,6 +5,9 @@ export CURRENT_PROGRAM="sbs"
 
 export SUBSTANCE_PLUGINS_DIR=${MEDIA_PROJECT_DIR}'/pipe/tools/sbstools/_custom/'
 export SUBSTANCE_ICONS_DIR=${MEDIA_PROJECT_DIR}'/pipe/tools/_resources/'
+export PYTHONPATH=${PYTHONPATH}:${MEDIA_PROJECT_DIR}
+
+cp -ar ${MEDIA_PROJECT_DIR}'/pipe/tools/sbstools' ~/Documents/Allegorithmic/Substance\ Painter/plugins/
 
 echo "Starting Substance..."
 substancepainter
