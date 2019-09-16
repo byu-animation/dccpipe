251f50a337dfb33853f216e517353898b4bea0f7
Nathaniel Hebbert <nwh19@opensuse.cs.byu.edu>
2019-09-16 15:43:28 -0600

    chore: add settings.js for constant values used throughout sbstools #94

---
+++ pipe/tools/sbstools/settings.js
@@ -0,0 +1,7 @@
+// settings.js
+.pragma library
+
+var pathToAssets = "/production/assets/"
+var pathToTextures = "/texture/main/"
+var pathToCache = pathToTextures + "cache/"
+var fbxPostfix = "_texture_main.fbx"
