17c0530c4eb30ff04ee143160d84bf64d5230ed1
Matthew Swindler <mattheweswindler@gmail.com>
2019-11-08 17:49:27 -0700

    Fix #133

--- update_project.py
+++ update_project.py
@@ -1,61 +1,61 @@
 import pipe.am.pipeline_io as pipeline_io
 import os
-import sys
 import json
 import subprocess
+import argparse
 
 
 '''
-Python script to initialize the production side of the pipe.
-Usage - python create_project.py nameOfProject nicknameOfProject
+Script used to determine what aspects of dccpipe to update
 '''
-def create_project():
-    # if not len(sys.argv) == 3:
-    #     print("Create project failed. Invalid number of arguments.")
-    #     print("Usage - python create_project.py nameOfProject nicknameOfProject")
-    #     return
-    # project_dir = os.getenv("MEDIA_PROJECT_DIR")
-    #
-    # # get the command line arguments 1 - name, 2 - nickname
-    # name = sys.argv[1]
-    # nickname = sys.argv[2]
-    # modify_project_config(name, nickname)
-    #
-    # pipe_dict = pipeline_io.readfile(".project")
+def get_arguments_and_run():
+    parser = argparse.ArgumentParser(description='Usage: python update_project.py --shortcuts --name myName --nickname myNick --email myEmail')
+    parser.add_argument("--shortcuts", "-sc", action="store_true", help="If present, shortcuts will be regenerated.")
+    parser.add_argument("--name", "-n", type=str, help="If present, update the name of the Project in the .project file with the specified value.")
+    parser.add_argument("--nickname", "-nn", type=str, help="If present, update the nickname of the Project in the .project file with the specified value.")
+    parser.add_argument("--email", "-e", type=str, help="If present, update the email of the Project in the .project file with the specified value.")
 
-    # pipeline_io.mkdir(pipe_dict["production_dir"])
-    # pipeline_io.mkdir(pipe_dict["assets_dir"])
-    # pipeline_io.mkdir(pipe_dict["crowds_dir"])
-    # pipeline_io.mkdir(pipe_dict["shots_dir"])
-    # pipeline_io.mkdir(pipe_dict["tools_dir"])
-    # pipeline_io.mkdir(pipe_dict["users_dir"])
-    # pipeline_io.mkdir(pipe_dict["hda_dir"])
-    # pipeline_io.mkdir(pipe_dict["reference_dir"])
+    args = parser.parse_args()
+    shortcuts = args.shortcuts
+    name = args.name
+    nickname = args.nickname
+    email = args.email
 
-    pipe_dict = pipeline_io.readfile(".project")
+    if shortcuts == False and name == None and nickname == None and email == None:
+        print('Usage: python update_project.py --shortcuts --name myName --nickname myNick --email myEmail')
+        return
 
-    create_project_shortcuts(nickname=pipe_dict['nickname'], name=pipe_dict['name'])
+    if name:
+        modify_project_config("name", name)
+    if nickname:
+        modify_project_config("nickname", nickname)
+    if email:
+        modify_project_config("email_address", email)
+    if shortcuts:
+        create_project_shortcuts()
 
     print("Production project successfully updated!")
 
-# def modify_project_config(name, nickname):
-#         with open(".project", "r") as jsonFile:
-#             data = json.load(jsonFile)
-#
-#         # tmp = data["name"]
-#         data["name"] = name
-#         data["nickname"] = nickname
-#
-#         with open(".project", "w") as jsonFile:
-#             json.dump(data, jsonFile)
+def modify_project_config(key, value):
+        with open(".project", "r") as jsonFile:
+            data = json.load(jsonFile)
 
-def create_project_shortcuts(nickname="test", name="test"):
+        data[key] = value
+
+        with open(".project", "w") as jsonFile:
+            json.dump(data, jsonFile)
+
+def create_project_shortcuts():
+    pipe_dict = pipeline_io.readfile(".project")
+    nickname = pipe_dict['nickname']
+    name = pipe_dict['name']
+
     cwd = os.getcwd()
     icon_script = os.path.join(cwd, 'create_project_shortcuts.sh')
 
-    if(nickname is not None):
+    if nickname is not None or nickname is not "":
         subprocess.call(['sh', icon_script, '-n', nickname, name, cwd])
     else:
         subprocess.call(['sh', icon_script, name, cwd])
 
-create_project()
+get_arguments_and_run()
